c65f8a4e84eb0fb56b5fc4a79dc3e3e429738a57
rahul-dhiman9 <rahuldhiman444455555@gmail.com>
2026-09-12 03:53:29 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -1,17 +1,40 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use("/test",(req,res)=>{
-  res.send("Hello ji")
-})
+// app.use("/user",(req,res)=>{
+//   console.log("hahahaha");
+// })
+
+//this will only handle get call to /user
+app.get("/user", (req, res) => {
+  res.send({ name: "Rahul" });
+});
+app.post("/user", (req, res) => {
+  res.send("data saved successfully");
+});
+app.put("/user", (req, res) => {
+  res.send("data updated successfully");
+});
+app.patch("/user", (req, res) => {
+  res.send("data partially successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("data deleted successfully");
+});
 
 
-app.use("/hello",(req,res)=>{
-  res.send("Hello ji hello")
-})
 
 
-app.listen(3002,()=>{
+// app.use("/", (req, res) => {
+//   res.send("Hello ji");
+// });
+
+//this will match all the http method to /test
+app.use("/test", (req, res) => {
+  res.send("Hello ji");
+});
+
+app.listen(3002, () => {
   console.log("server is started");
-})
+});
