3f35e45781c446168f693562015daf0372f846ec
rahul-dhiman9 <rahuldhiman444455555@gmail.com>
2026-09-13 03:23:47 +0530

    schemaValidations

--- src/app.js
+++ src/app.js
@@ -4,34 +4,116 @@ const User = require("./models/user");
 
 const app = express();
 
+app.use(express.json());
+// Parses requests with JSON payloads.,Express will automatically convert that JSON into a JavaScript object
+app.use(express.urlencoded());
+//Parses requests with URL‑encoded payloads (like HTML form submissions),handles form submissions with x-www-form-urlencoded.
+
 app.post("/signup", async (req, res) => {
-  const userObj = {
-    firstName: "MY",
-    lastName: "Kholi",
-    emailId: "vk@232gmai.com",
-    password: "vk12",
-  };
+    console.log(req.body);
+
   //creating a new instance of the user model
-  const user = new User(userObj);
+  const user = new User(req.body);
 
   try {
     await user.save();
 
-    // res.send("user added successfully")
+    res.send("user added successfully")
     res.send(userObj);
+
   } catch (err) {
     res.status(400).send("error saving user");
+  }
+});
+
+// //feed api get /feed get all the users from the db
+// //get user by email
+// app.get("/user",async(req,res)=>{
+//   try{
+//   const users = await User.find({emailId:req.body.emailId})
+//   res.send(users)
+//   console.log(users.length)
+//   }
+//   catch(err){
+//     res.status(404).send("something went wrong")
+//   }
+// })
+// //to get all the users
+// app.get("/feed",async(req,res)=>{
+//   try{
+//   const users = await User.find({})
+//   res.send(users)
+//   console.log(users.length)
+//   }
+//   catch(err){
+//     res.status(404).send("something went wrong")
+//   }
+// })
+// //to get ONE OF TWO the users
+// app.get("/oneuser",async(req,res)=>{
+//   try{
+//   const user = await User.findOne({emailId:req.body.emailId})
+//   if(!user){
+//     res.status(404).send("no user")
+//   }
+//   res.send(user)
+//   }
+//   catch(err){
+//     res.status(404).send("something went wrong")
+//   }
+// })
+
+// //to get by id the users
+// app.get("/iduser/:_id",async(req,res)=>{
+//   try{
+//     const id = req.params._id
+//   const user = await User.findById(id)
+//   if(!user){
+//     res.status(404).send("no user")
+//   }
+//   res.send(user)
+//   }
+//   catch(err){
+//     res.status(404).send("something went wrong")
+//   }
+// })
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("deleted successfully");
+    console.log("deleted");
+  } catch (err) {
+    res.status(400).send("error saving user");
+  }
+});
+
+//updating the data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const updateduser = await User.findByIdAndUpdate(userId , data,{
+      returnDocument:"after",
+      runValidators:true
+    });
+    res.send("updated successfully");
+    console.log(updateduser);
+    
+  } catch (err) {
+    res.status(400).send("error updating user");
   }
 });
 
 connectDb()
   .then(() => {
     console.log("database connected successfully");
+
+    app.listen(3002, () => {
+      console.log("Server is successfully listening on port 3002...");
+    });
   })
   .catch((err) => {
     console.log("database can not be connected");
   });
-
-app.listen(3002, () => {
-  console.log("Server is successfully listening on port 3002...");
-});
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,59 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  firstName:{
+  firstName: {
+    type: String,
+    required: true,
+    minLength:4,
+    maxLength:20,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+    required: true,
+    lowercase:true,
+    trim:true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    min:18,
+    max:100,
+    type: Number,
+  },
+  gender: {
+    validate(value){
+      if(!["male","female","others"].includes(value)){
+        throw new Error("gender is not specified",err.message)
+      } 
+    },
+    type: String,
+  },
+  photoUrl: {
+    type: String,
+    default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTro-iMp3rwLVEqt6I6NyOlW4JpSkSezwVXo1xiLPGSMQ&s=10"
+  },
+  about:{
     type:String,
+    default:"Just match na matachaa!",
+    minLength:20,
+    maxLength:200,
   },
-  lastName:{
-    type:String,
-  },
-  emailId:{
-    type:String,
-  },
-  password:{
-    type:String
-  },
-  age:{
-    type:Number
-  },
-  gender:{
-    type:String
+  skills:{
+    type:[String],
+    minLength:20,
+    maxLength:200,
   }
-})
+},
+{
+  timestamps:true
+}
+);
 
-const User = mongoose.model("User",userSchema)
-module.exports = User
+const User = mongoose.model("User", userSchema);
+module.exports = User;
