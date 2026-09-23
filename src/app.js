6b81a053a73a26a398c0881dc787bb869134f14b
rahul-dhiman9 <rahuldhiman444455555@gmail.com>
2026-09-23 23:07:21 +0530

    abcd

--- src/app.js
+++ src/app.js
@@ -14,39 +14,12 @@ const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
 const cors = require("cors");
 const path = require("path");
-const sendEmail = require("./utils/sendEmail");
+
 
 
 require("dotenv").config();
 const PORT = process.env.PORT 
 
-app.get("/test-email", async (req, res) => {
-  try {
-    const response = await sendEmail({
-      to: "raadevelopedit@gmail.com",
-      subject: "DevTinder SES Test",
-      text: "Hello! AWS SES is working.",
-      html: `
-        <h2>DevTinder SES Test</h2>
-        <p>Hello! AWS SES is working successfully.</p>
-      `,
-    });
-
-    console.log("SES response:", response);
-
-    res.json({
-      message: "Email sent successfully",
-      response,
-    });
-  } catch (error) {
-    console.error("SES email failed:", error);
-
-    res.status(500).json({
-      message: "Email failed",
-      error: error.message,
-    });
-  }
-});
 
 
 //multer ke liye,  Serve files from the local uploads folder at /uploads/<filename>
